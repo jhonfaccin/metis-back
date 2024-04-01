322afafd26b64684c4268eb05af3294a173d87ba
jhon <jhonfaccin@gmail.com>
2024-04-01 14:34:09 -0300

    add body-parser to project

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 9000;
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use("/metis",require("./server/route/metisRoute"));
 
 
